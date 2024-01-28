df206d4c8448652e6b0601f75193b8200efd94cc
GDRead3 <141354136+GDRead3@users.noreply.github.com>
2024-01-28 12:54:44 -0700

    Update page.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,13 @@
-export default function Page() {
+import StudentInfo from "C:cprg306cprg306-assignmentsappweek-2/student-info";
+
+const Week2Page = () => {
   return (
-    <main>
-      <h1>Shopping List</h1>
-    </main>
+    <div>
+      <h1>Week 2 Page</h1>
+      <StudentInfo />
+      {}
+    </div>
   );
-}
+};
+
+export default Week2Page;
